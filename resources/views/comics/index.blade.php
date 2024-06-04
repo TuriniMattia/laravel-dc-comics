99142c6bc8d5a6096cbc661e505623ab20dd95cd
TuriniMattia <turinimattia96@gmail.com>
2024-06-04 21:01:38 +0200

    [add]index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,1 +1,46 @@
+@extends('layout.app')
+
+@section('title','Nuovi arrivi')
+
+@section('content')
 <h1>vista index di _d_c_comics</h1>
+<main>
+    <section>
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Image</th>
+                        <th>Price</th>
+                        <th>Series</th>
+                        <th>Sale Date</th>
+                        <th>Type</th>
+                        <th>Artist</th>
+                        <th>Writer</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($comics as $comic)
+                    <tr>
+                        <td>{{$comic->title}}</td>
+                        <td>{{$comic->description}}</td>
+                        <td>
+                            <img src="{{ $comic->thumb }}" height="150" width="150" alt="">
+                        </td>
+                        <td>{{$comic->price}}</td>
+                        <td>{{$comic->series}}</td>
+                        <td>{{$comic->sale_date}}</td>
+                        <td>{{$comic->type}}</td>
+                        <td>{{$comic->artist}}</td>
+                        <td>{{$comic->writer}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </section>
+</main>
+
+@endsection
